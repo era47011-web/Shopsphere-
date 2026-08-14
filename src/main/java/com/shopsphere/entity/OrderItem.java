8f73851f70a6fc5c4fc288139f6c289d9e5e49d8
era47011-web <era47011@gmail.com>
2026-08-15 00:35:45 +0530

    Create OrderItem.java

---
+++ src/main/java/com/shopsphere/entity/OrderItem.java
@@ -0,0 +1,61 @@
+package com.shopsphere.entity;
+
+public class OrderItem {
+
+    private Long id;
+    private Long orderId;
+    private Long productId;
+    private Integer quantity;
+    private Double price;
+
+    public OrderItem() {
+    }
+
+    public OrderItem(Long id, Long orderId, Long productId, Integer quantity, Double price) {
+        this.id = id;
+        this.orderId = orderId;
+        this.productId = productId;
+        this.quantity = quantity;
+        this.price = price;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(Long orderId) {
+        this.orderId = orderId;
+    }
+
+    public Long getProductId() {
+        return productId;
+    }
+
+    public void setProductId(Long productId) {
+        this.productId = productId;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+}
