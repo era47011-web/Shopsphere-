8064e1e53eebf6aefa5fc8f54e11bb8507bbf5e3
era47011-web <era47011@gmail.com>
2026-08-15 00:57:25 +0530

    Create OrderItemRepository.java

---
+++ src/main/java/com/shopsphere/OrderItemRepository.java
@@ -0,0 +1,7 @@
+package com.shopsphere.repository;
+
+import com.shopsphere.entity.OrderItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
+}
