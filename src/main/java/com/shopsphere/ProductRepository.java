831ffeefe7e8e215f7f39b9c4f5307f13cbfea5b
era47011-web <era47011@gmail.com>
2026-08-15 00:53:15 +0530

    Create ProductRepository.java

---
+++ src/main/java/com/shopsphere/ProductRepository.java
@@ -0,0 +1,7 @@
+package com.shopsphere.repository;
+
+import com.shopsphere.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
