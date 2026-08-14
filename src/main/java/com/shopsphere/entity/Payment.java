b4f5235207a108a032e8588cc739d916eb3d17cc
era47011-web <era47011@gmail.com>
2026-08-15 00:38:58 +0530

    Create Payment.java

---
+++ src/main/java/com/shopsphere/entity/Payment.java
@@ -0,0 +1,72 @@
+package com.shopsphere.entity;
+
+public class Payment {
+
+    private Long id;
+    private Long orderId;
+    private Double amount;
+    private String paymentMethod;
+    private String paymentStatus;
+    private String paymentDate;
+
+    public Payment() {
+    }
+
+    public Payment(Long id, Long orderId, Double amount,
+                   String paymentMethod, String paymentStatus, String paymentDate) {
+        this.id = id;
+        this.orderId = orderId;
+        this.amount = amount;
+        this.paymentMethod = paymentMethod;
+        this.paymentStatus = paymentStatus;
+        this.paymentDate = paymentDate;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(Long orderId) {
+        this.orderId = orderId;
+    }
+
+    public Double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Double amount) {
+        this.amount = amount;
+    }
+
+    public String getPaymentMethod() {
+        return paymentMethod;
+    }
+
+    public void setPaymentMethod(String paymentMethod) {
+        this.paymentMethod = paymentMethod;
+    }
+
+    public String getPaymentStatus() {
+        return paymentStatus;
+    }
+
+    public void setPaymentStatus(String paymentStatus) {
+        this.paymentStatus = paymentStatus;
+    }
+
+    public String getPaymentDate() {
+        return paymentDate;
+    }
+
+    public void setPaymentDate(String paymentDate) {
+        this.paymentDate = paymentDate;
+    }
+}
