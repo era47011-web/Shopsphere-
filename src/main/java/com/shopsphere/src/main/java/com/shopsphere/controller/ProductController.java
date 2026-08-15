c0c48985edcafe136baf797c01f989d53b4063f4
era47011-web <era47011@gmail.com>
2026-08-15 15:15:30 +0530

    Create controller/ProductController.java

---
+++ src/main/java/com/shopsphere/src/main/java/com/shopsphere/controller/ProductController.java
@@ -0,0 +1,38 @@
+package com.shopsphere.controller;
+
+import com.shopsphere.entity.Product;
+import com.shopsphere.service.ProductService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/products")
+public class ProductController {
+
+    private final ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping
+    public List<Product> getAllProducts() {
+        return productService.getAllProducts();
+    }
+
+    @GetMapping("/{id}")
+    public Product getProductById(@PathVariable Long id) {
+        return productService.getProductById(id);
+    }
+
+    @PostMapping
+    public Product addProduct(@RequestBody Product product) {
+        return productService.saveProduct(product);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteProduct(@PathVariable Long id) {
+        productService.deleteProduct(id);
+    }
+}
