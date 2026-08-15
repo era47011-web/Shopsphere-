67ac3188899babb1774291856de61209b727cbf1
era47011-web <era47011@gmail.com>
2026-08-15 13:52:28 +0530

    Create src/main/java/com/shopsphere/service/UserService.java

---
+++ src/main/java/com/shopsphere/src/main/java/com/shopsphere/service/UserService.java
@@ -0,0 +1,19 @@
+package com.shopsphere.service;
+
+import com.shopsphere.entity.User;
+import com.shopsphere.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public User saveUser(User user) {
+        return userRepository.save(user);
+    }
+}
