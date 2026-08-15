40f1d1bc94cc51826332039ce4a6bb0d6ecce0e6
era47011-web <era47011@gmail.com>
2026-08-15 14:05:00 +0530

    Create OrderService.java

---
+++ src/main/java/com/shopsphere/service/OrderService.java
@@ -0,0 +1,19 @@
+package com.shopsphere.service;
+
+import com.shopsphere.entity.Order;
+import com.shopsphere.repository.OrderRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class OrderService {
+
+    private final OrderRepository orderRepository;
+
+    public OrderService(OrderRepository orderRepository) {
+        this.orderRepository = orderRepository;
+    }
+
+    public Order saveOrder(Order order) {
+        return orderRepository.save(order);
+    }
+}
