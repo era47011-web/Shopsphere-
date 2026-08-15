336883874ca67c01b86b3fd2f6bd0e310fce7ee7
era47011-web <era47011@gmail.com>
2026-08-15 14:18:00 +0530

    Create controller/UserController.java

---
+++ src/main/java/com/shopsphere/controller/UserController.java
@@ -0,0 +1,21 @@
+package com.shopsphere.controller;
+
+import com.shopsphere.entity.User;
+import com.shopsphere.service.UserService;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping
+    public User createUser(@RequestBody User user) {
+        return userService.saveUser(user);
+    }
+}
