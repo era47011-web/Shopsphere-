0376901c008689d27cffddd814fe680eda019182
era47011-web <era47011@gmail.com>
2026-08-15 15:18:43 +0530

    Create UserController.java

---
+++ src/main/java/com/shopsphere/src/main/java/com/shopsphere/controller/UserController.java
@@ -0,0 +1,45 @@
+package com.shopsphere.controller;
+
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+import com.shopsphere.entity.User;
+import com.shopsphere.service.UserService;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userService.getAllUsers();
+    }
+
+    @GetMapping("/{id}")
+    public User getUserById(@PathVariable Long id) {
+        return userService.getUserById(id);
+    }
+
+    @PostMapping
+    public User createUser(@RequestBody User user) {
+        return userService.saveUser(user);
+    }
+
+    @PutMapping("/{id}")
+    public User updateUser(@PathVariable Long id,
+                           @RequestBody User user) {
+        return userService.updateUser(id, user);
+    }
+
+    @DeleteMapping("/{id}")
+    public String deleteUser(@PathVariable Long id) {
+        userService.deleteUser(id);
+        return "User deleted successfully";
+    }
+}
