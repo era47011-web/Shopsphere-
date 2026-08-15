55b91541f94808b38b31459af801e501a5636854
era47011-web <era47011@gmail.com>
2026-08-15 13:56:34 +0530

    Create ProductService.java

---
+++ src/main/java/com/shopsphere/src/main/java/com/shopsphere/service/ProductService.java
@@ -0,0 +1,19 @@
+package com.shopsphere.service;
+
+import com.shopsphere.entity.Product;
+import com.shopsphere.repository.ProductRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductService {
+
+    private final ProductRepository productRepository;
+
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    public Product saveProduct(Product product) {
+        return productRepository.save(product);
+    }
+}
