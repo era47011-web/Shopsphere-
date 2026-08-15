71c15123cc9ce624667f607fb4e155d7ccea8b87
era47011-web <era47011@gmail.com>
2026-08-15 15:36:35 +0530

    Create PaymentService.java

---
+++ src/main/java/com/shopsphere/src/main/java/com/shopsphere/service/PaymentService.java
@@ -0,0 +1,38 @@
+package com.shopsphere.service;
+
+import org.springframework.stereotype.Service;
+import java.util.List;
+import java.util.Optional;
+
+import com.shopsphere.entity.Payment;
+import com.shopsphere.repository.PaymentRepository;
+
+@Service
+public class PaymentService {
+
+    private final PaymentRepository paymentRepository;
+
+    public PaymentService(PaymentRepository paymentRepository) {
+        this.paymentRepository = paymentRepository;
+    }
+
+    public List<Payment> getAllPayments() {
+        return paymentRepository.findAll();
+    }
+
+    public Optional<Payment> getPaymentById(Long id) {
+        return paymentRepository.findById(id);
+    }
+
+    public Payment savePayment(Payment payment) {
+        return paymentRepository.save(payment);
+    }
+
+    public Payment updatePayment(Payment payment) {
+        return paymentRepository.save(payment);
+    }
+
+    public void deletePayment(Long id) {
+        paymentRepository.deleteById(id);
+    }
+}
