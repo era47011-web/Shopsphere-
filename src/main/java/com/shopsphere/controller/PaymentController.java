cb94a1fc8490348f7ba47418bdca74efe17fcf13
era47011-web <era47011@gmail.com>
2026-08-15 14:25:46 +0530

    Create PaymentController.java

---
+++ src/main/java/com/shopsphere/controller/PaymentController.java
@@ -0,0 +1,21 @@
+package com.shopsphere.controller;
+
+import com.shopsphere.entity.Payment;
+import com.shopsphere.service.PaymentService;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/payments")
+public class PaymentController {
+
+    private final PaymentService paymentService;
+
+    public PaymentController(PaymentService paymentService) {
+        this.paymentService = paymentService;
+    }
+
+    @PostMapping
+    public Payment createPayment(@RequestBody Payment payment) {
+        return paymentService.savePayment(payment);
+    }
+}
