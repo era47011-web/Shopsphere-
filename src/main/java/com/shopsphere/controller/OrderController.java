17d5c1ed4f8a642a8e7377ff099b0b7e26b868a6
era47011-web <era47011@gmail.com>
2026-08-15 14:23:40 +0530

    Create OrderController.java

---
+++ src/main/java/com/shopsphere/controller/OrderController.java
@@ -0,0 +1,21 @@
+package com.shopsphere.controller;
+
+import com.shopsphere.entity.Order;
+import com.shopsphere.service.OrderService;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/orders")
+public class OrderController {
+
+    private final OrderService orderService;
+
+    public OrderController(OrderService orderService) {
+        this.orderService = orderService;
+    }
+
+    @PostMapping
+    public Order createOrder(@RequestBody Order order) {
+        return orderService.saveOrder(order);
+    }
+}
